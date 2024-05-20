360ff2dcafa0f8a678afa98d2e2c28562b2d42ca
lixinhui <lixinhui@agora.io>
2024-05-20 16:56:28 +0800

    chore(): init

---
+++ Formula/buzz-cli.rb
@@ -0,0 +1,29 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class BuzzCli < Formula
+  desc 'Buzz CLI'
+  homepage ''
+  url 'https://github.com/sunshinexcode/buzz-cli/releases/download/v0.1.0/buzz-cli-macos-x64.tar.gz'
+  sha256 'e24fcdd4bd37d51b19d21c96307720c41d76f8eeb62f897bf126f9806e53a736'
+  license 'MIT'
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install 'buzz-cli'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tpc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system 'false'
+  end
+end
---
+++ README.md
@@ -0,0 +1,26 @@
+# homebrew-buzz-cli
+
+```sh
+$ brew tap sunshinexcode/homebrew-buzz-cli
+```
+
+## buzz-cli
+
+### Install
+
+```sh
+$ brew tap sunshinexcode/homebrew-buzz-cli
+$ brew install buzz-cli
+```
+
+或者
+
+```sh
+$ brew install sunshinexcode/buzz-cli/buzz-cli
+```
+
+### upgrade
+
+```sh
+$ brew upgrade buzz-cli
+```
